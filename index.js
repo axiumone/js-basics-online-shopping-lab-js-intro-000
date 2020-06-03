43a3c5439d1e90a125fe753f91efcb0c7299573e
Dmitry Sychev <axiumone@gmail.com>
2020-06-03 14:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var current_cart = [`In your cart, you have`];
+  var current_cart = "In your cart, you have"
   if(cart.length === 0) {
     return "Your shopping cart is empty.";
   } else if (cart.length === 1) {
