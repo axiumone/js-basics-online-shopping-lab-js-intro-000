594db68b35c4f7a5119eb2a5090f52f93c76aaa1
Dmitry Sychev <axiumone@gmail.com>
2020-06-03 14:21:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,9 +63,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   return (cardNumber === undefined ) ? "Sorry, we don't have a credit card on file for you."
-    : `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`; }
-
-  {  cart.length = 0;
+    : cart.length = 0, `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   // if(cardNumber === undefined ) {
   //   return "Sorry, we don't have a credit card on file for you.";
   // } else {
